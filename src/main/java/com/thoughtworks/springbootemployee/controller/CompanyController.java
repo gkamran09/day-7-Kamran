43b183d0ef3b3696489263315db2a13617161edd
Gerard Kamran Kalambacal <kamrangerardkalambacal@gmail.com>
2023-08-22 19:22:22 +0800

    feat: created Company controller and repository

---
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -0,0 +1,10 @@
+package com.thoughtworks.springbootemployee.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping(path = "/companies")
+@RestController
+public class CompanyController {
+
+}
