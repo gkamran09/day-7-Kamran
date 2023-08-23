c968d2b12a7b41fb5ba86ae3dd4e4cc9f2e56cf7
Gerard Kamran Kalambacal <kamrangerardkalambacal@gmail.com>
2023-08-23 18:50:09 +0800

    test: added test case should_return_all_companies_when_perform_get_companies

--- src/test/java/com/thoughtworks/springbootemployee/CompanyApiTests.java
+++ src/test/java/com/thoughtworks/springbootemployee/CompanyApiTests.java
@@ -1,11 +1,18 @@
 package com.thoughtworks.springbootemployee;
 
+import com.thoughtworks.springbootemployee.model.Company;
 import com.thoughtworks.springbootemployee.repository.CompanyRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -19,5 +26,18 @@ public class CompanyApiTests {
     @BeforeEach
     void cleanupCompanyData() {
         companyRepository.cleanAll();
+    }
+
+    @Test
+    void should_return_all_companies_when_perform_get_companies() throws Exception {
+        // Given
+        Company company = companyRepository.saveCompany(new Company(1L,"OOCL"));
+
+        // When, Then
+        mockMvc.perform(MockMvcRequestBuilders.get("/companies"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].companyId").value(company.getCompanyId()))
+                .andExpect(jsonPath("$[0].companyName").value(company.getCompanyName()));
     }
 }
